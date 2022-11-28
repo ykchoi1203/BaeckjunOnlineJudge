64a32e93e4a0ffab0189088b5854da51f1471162
choiyounggeun <30820741+ykchoi1203@users.noreply.github.com>
2022-11-28 23:11:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 104.10 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         // Arrays.sort(participant);
@@ -6,12 +8,13 @@ class Solution {
         // for(int i=0; i<completion.length; i++) {
         //     if(!completion[i].equals(participant[i])) return participant[i];
         // }
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String name : participant) map.put(name, map.getOrDefault(name, 0) + 1);
+        Map<String, Long> map = Arrays.stream(participant).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        // HashMap<String, Integer> map = new HashMap<>();
+        // for(String name : participant) map.put(name, map.getOrDefault(name, 0) + 1);
         for(String name : completion) map.put(name, map.get(name) -1);
 
-        Set<Map.Entry<String, Integer>> entry = map.entrySet();
-        for(Map.Entry<String, Integer> el : entry){
+        Set<Map.Entry<String, Long>> entry = map.entrySet();
+        for(Map.Entry<String, Long> el : entry){
             if(el.getValue() != 0) return el.getKey();
         }
         return participant[participant.length-1];
