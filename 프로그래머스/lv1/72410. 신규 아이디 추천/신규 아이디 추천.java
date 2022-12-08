e6d979b5869d1e545999a737a8aa0bdbd6f3a90d
choiyounggeun <30820741+ykchoi1203@users.noreply.github.com>
2022-12-08 20:51:01 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.28 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -4,7 +4,6 @@ class Solution {
         new_id = new_id.toLowerCase();
 		new_id = new_id.replaceAll("[^a-z0-9\\._-]", "");
 		new_id = new_id.replaceAll("\\.+", ".");
-		new_id = new_id.replaceAll("\\.+", ".");
 		new_id = new_id.replaceAll("^[.]|[.]$", "");
 		if(new_id.length() == 0)
 			new_id = "a";
