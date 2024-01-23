26820f6d4c7e072369782766d3b41348b0050f9f
choiyounggeun <30820741+ykchoi1203@users.noreply.github.com>
2024-01-23 22:00:48 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 5156 ms, Memory: 311192 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.java
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.java
@@ -1,21 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 
 
 		for(int i=0; i<n; i++) {
-			int num = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			
 			long[] arr = new long[num];
-			for(int j=0; j<num; j++) {
-				arr[j]=sc.nextLong();
+			int idx = 0;
+			while(st.hasMoreElements()) {
+				arr[idx] = Long.parseLong(st.nextToken());
+                idx++;
 			}
 			sb.append(answer(arr));
 			sb.append("\n");
