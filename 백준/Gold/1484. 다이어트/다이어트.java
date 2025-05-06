97e4cb02a1106a0ffc42e13b51d8312f398a39e5
choiyounggeun <30820741+ykchoi1203@users.noreply.github.com>
2025-05-06 23:46:25 +0900

    [Gold V] Title: 다이어트, Time: 180 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -2,39 +2,31 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+               Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         int cur = 1;
         int last = 1;
-
-        TreeSet<Integer> set = new TreeSet<>();
-
-        while(true) {
-            if(last == cur + 1 && last * last - cur * cur > n) break;
-
-            if(last * last - cur * cur == n) {
-                set.add(last);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (last != cur + 1 || last * last - cur * cur <= n) {
+            if (last * last - cur * cur == n) {
+                sb.append(last).append("\n");
                 cur++;
                 last = cur + 1;
-            } else if(last * last - cur * cur < n ) {
+            } else if (last * last - cur * cur < n) {
                 last++;
             } else {
                 cur++;
                 last = cur + 1;
             }
         }
-
-        if(set.isEmpty()) {
+        
+        if(sb.length() == 0) {
             System.out.println(-1);
             return;
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i : set) {
-            sb.append(i).append("\n");
         }
 
         System.out.println(sb);
