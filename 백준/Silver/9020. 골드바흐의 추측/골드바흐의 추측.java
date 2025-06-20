3bde818f51850dd7f5f3f9a3c2c0d366375a5549
choiyounggeun <30820741+ykchoi1203@users.noreply.github.com>
2025-06-21 01:03:08 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 336 ms, Memory: 24908 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[10001];
+
+        boolean[] prime = new boolean[10001];
+
+        prime[2] = true;
+
+        for(int i = 3; i <= 10000; i+=2) {
+            if(isPrime(i)) {
+                prime[i] = true;
+            }
+        }
+
+        for(int i = 4; i <= 10000; i+=2) {
+            if(prime[i - 2]) {
+                arr[i] = 2;
+            }
+
+            for(int j = 3; j <= i / 2; j+=2) {
+                if(prime[j] && prime[i - j]) {
+                    arr[i] = j;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(n-- > 0) {
+            int num = sc.nextInt();
+
+            sb.append(arr[num]).append(" ").append(num - arr[num]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static boolean isPrime(int n) {
+        for(int i = 3; i <= Math.sqrt(n); i += 2) {
+            if(n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+}
